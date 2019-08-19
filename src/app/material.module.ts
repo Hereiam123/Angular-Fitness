34596360c4783f0abdb52220b8adab71702827f1
hereiam123 <bdemaio1@gmail.com>
2019-08-19 00:00:30 -0400

    Updating form with datepicker for user birthdate

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,27 @@ import { MatButtonModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ]
 })
 export class MaterialModule {}
