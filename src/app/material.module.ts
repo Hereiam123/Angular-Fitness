0c01c2091aed7511599241d2b6553f132314ac47
hereiam123 <bdemaio1@gmail.com>
2019-08-18 22:22:43 -0400

    Adding in some form elements for the signup form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule {}
