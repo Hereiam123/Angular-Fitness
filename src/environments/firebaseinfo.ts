123b65e301a1d0ee949e983c00b417328f1d5650
Brian De Maio <bdemaio1@gmail.com>
2019-12-14 18:56:36 -0500

    A little update

---
+++ src/environments/firebaseinfo.ts
@@ -0,0 +1,11 @@
+const FirebaseInfo = {
+    apiKey: "AIzaSyBpgScZG4HO-sEu31PKnzsb1XJ8pvijkeY",
+    authDomain: "angular-fitness-ea2bd.firebaseapp.com",
+    databaseURL: "https://angular-fitness-ea2bd.firebaseio.com",
+    projectId: "angular-fitness-ea2bd",
+    storageBucket: "angular-fitness-ea2bd.appspot.com",
+    messagingSenderId: "1064349192849",
+    appId: "1:1064349192849:web:b94dbda8331dacbe4f2530"
+}
+
+export default FirebaseInfo;
